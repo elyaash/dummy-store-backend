e74d93cd7bc782d80a65387685eabc1a52e8dc1a
prajput <prajput@pch.com>
2024-12-19 15:17:58 -0500

    Fix the pug template path issue

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req: Request, res:Response, next: NextFunction) {
-  //res.render('index', { title: 'Express' });
-  res.send("This is homepage ");
+  res.render('index', { title: 'Express' });
+  //res.send("This is homepage ");
 });
 
 export default router;
