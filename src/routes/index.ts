f9665046f0c8adae20cc6cc01b20879b9507cff6
Elyaash <88165102+elyaash@users.noreply.github.com>
2025-04-28 10:01:27 -0500

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 
-var router = express.Router();
+const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req: Request, res: Response, next: NextFunction) {
